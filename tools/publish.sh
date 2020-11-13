e7a65316fe6210bba9566439e4efd807aa9838a6
linchen1987 <linchen.1987@foxmail.com>
2020-11-13 15:39:28 +0800

    fix: ci

--- tools/publish.sh
+++ tools/publish.sh
@@ -18,6 +18,31 @@ make release
 
 node tools/post-publish.js
 
+# deploy to remote ABT Node
+set +e
+NAME=$(cat package.json | grep name | head -n 1 |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
+VERSION=$(cat package.json | grep version | head -n 1 |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
+if [ "${ALIYUN_NODE_ENDPOINT}" != "" ]; then
+  abtnode deploy .blocklet/bundle --endpoint ${ALIYUN_NODE_ENDPOINT} --access-key ${ALIYUN_NODE_ACCESS_KEY} --access-secret ${ALIYUN_NODE_ACCESS_SECRET} --skip-hooks
+  if [ $? == 0 ]; then
+    echo "deploy to ${ALIYUN_NODE_ENDPOINT} success"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${ALIYUN_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+  else
+    echo "deploy to ${ALIYUN_NODE_ENDPOINT} failed"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${ALIYUN_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+  fi
+fi
+if [ "${AWS_NODE_ENDPOINT}" != "" ]; then
+  abtnode deploy .blocklet/bundle --endpoint ${AWS_NODE_ENDPOINT} --access-key ${AWS_NODE_ACCESS_KEY} --access-secret ${AWS_NODE_ACCESS_SECRET} --skip-hooks
+  if [ $? == 0 ]; then
+    echo "deploy to ${AWS_NODE_ENDPOINT} success"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${AWS_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+  else
+    echo "deploy to ${AWS_NODE_ENDPOINT} failed"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${AWS_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
+  fi
+fi
+
 # trigger ArcBlock/blocklets repo release
 echo "trigger ArcBlock/blocklets repo release"
 .makefiles/trigger_registry_build.sh
